785e43c39cac8e4d264beebe0a65658ab01d1930
Greg Deane <greg.deane.dev@gmail.com>
2016-05-25 17:07:47 +0200

    refactor(provider): wrote most methods for environments provider

--- src/environments-provider.js
+++ src/environments-provider.js
@@ -1,30 +1,86 @@
 angular.module('solution.center.communicator')
 
-  .provider('environments', ['ENVIRONMENTS', function (ENVIRONMENTS) {
-    'use strict';
+  .provider('environments', ['$windowProvider', 'ENVIRONMENTS',
+    function ($windowProvider, ENVIRONMENTS) {
+      'use strict';
 
-    var getEnvironment = function (config) {
-      if (typeof window['jasmine'] == 'object') {
-        return config.TESTING;
-      }
+      var $window = $windowProvider.$get();
+      var isTestEnvironment = angular.isObject($window['jasmine']);
+      var environment;
 
-      var env = config.ENVIRONMENT;
-
-      // if "$" is NOT contained in the `NAME` value, use that environment
-      // if "$" is contained in the `NAME` value, use LOCAL environment
-      return (env.NAME.indexOf('$') === -1 && env) || config.LOCAL;
-
-    };
-
-    this.getCurrent = function (name) {
-      //return (custom && getEnvironment(custom)) || getEnvironment(CONFIG);
-    };
-
-    this.$get = function () {
-      return {
-        getCurrent: this.getCurrent
+      /**
+       * Get environment object by name
+       * Possible values: 'PRODUCTION', 'STAGE', 'INTEGRATION', 'DEVELOPMENT'
+       *
+       * @param {string} name - Environment name
+       * @returns {Object} Specified environment or fallback environment (LOCAL)
+       */
+      var getNamedEnvironment = function (name) {
+        return ENVIRONMENTS[name] || ENVIRONMENTS.LOCAL;
       };
-    };
 
-    return this;
-  }]);
+      /**
+       * Get environment object based on custom config
+       *
+       * The config object passed to this method must contain an `ENVIRONMENT` object to avoid falling back
+       * to `LOCAL`. The idea here is that the `ENVIRONMENT` object will have been populated with the necessary
+       * values prior to making a call to the `getCurrentEnvironment()` method.
+       *
+       * For example, Solution Center populates the `ENVIRONMENT` object via YAML files during deployment. The
+       * `NAME` property of this object will contain either the YAML-supplied environment name (e.g. "PRODUCTION")
+       * or the placeholder string "${NAME}".
+       *
+       * If we are in a non-LOCAL environment, "${NAME}" will have been replaced with the appropriate value from
+       * the YAML file.
+       *
+       * This method first checks that the `ENVIRONMENT` property exists in the custom config object. If `ENVIRONMENT`
+       * is found, `name` will be set to the value in the `NAME` property. Otherwise, `name` will be an empty string.
+       *
+       * The return statement ensures that `name` is neither empty nor contains a "$" sign. If both checks are true,
+       * the custom environment is returned. Otherwise, the fallback environment is returned.
+       *
+       * @param {Object} config - Custom environment config
+       * @returns {Object} Custom environment or fallback environment (LOCAL)
+       */
+      var getCustomEnvironment = function (config) {
+        var env = config && config.ENVIRONMENT;
+        var name = (env && env.NAME) || '';
+
+        return (name.length && name.indexOf('$') === -1 && env) || ENVIRONMENTS.LOCAL;
+      };
+
+      return {
+
+        /**
+         * Set current environment
+         * @param (string|Object} env - Environment name or custom environment object
+         * @returns {Object} Test environment, custom environment, or fallback environment
+         */
+        setCurrentEnvironment: function (env) {
+          // if `env` is a string, set that environment. otherwise, set custom environment
+          environment = (angular.isString(env) && getNamedEnvironment(env)) || getCustomEnvironment(env);
+
+          return (isTestEnvironment && ENVIRONMENTS.TESTING) || environment;
+        },
+
+        /**
+         * Get current environment
+         * If not previously configured, fallback to LOCAL
+         *
+         * @returns {Object} Current or LOCAL environment
+         */
+        getCurrentEnvironment: function () {
+          return environment || ENVIRONMENTS.LOCAL;
+        },
+
+        $get: function () {
+          return {
+            setCurrentEnvironment: this.setCurrentEnvironment,
+            getCurrentEnvironment: this.getCurrentEnvironment
+          };
+        }
+
+      };
+
+    }
+  ]);
