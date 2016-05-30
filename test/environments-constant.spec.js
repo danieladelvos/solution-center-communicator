48fb4e41934d04ce766a881e61300021169fa71e
Greg Deane <greg.deane.dev@gmail.com>
2016-05-30 08:24:23 +0200

    test(env): add test for environment properties

--- test/environments-constant.spec.js
+++ test/environments-constant.spec.js
@@ -1,6 +1,7 @@
 describe('environments constant', function () {
 
   var environmentNames = ['PRODUCTION', 'STAGE', 'INTEGRATION', 'DEVELOPMENT', 'LOCAL', 'TESTING'];
+  var environmentProps = ['NAME', 'URL', 'DOMAIN', 'PORT', 'MERCHANT_SERVICE', 'USER_SERVICE', 'TOKEN_SERVICE'];
   var config;
 
   beforeEach(function () {
@@ -13,6 +14,16 @@ describe('environments constant', function () {
       var env = config[name];
       expect(env).toEqual(jasmine.any(Object));
       expect(env.NAME).toBe(name);
+    });
+  });
+
+  it('should contain all environment properties', function () {
+    environmentNames.forEach(function (name) {
+      var env = config[name];
+
+      environmentProps.forEach(function (prop) {
+        expect(env[prop]).toBeDefined();
+      });
     });
   });
 
